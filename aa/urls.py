834d2689e1b1ee21289d96fdf4ef1c0209b15a27
ocxh <gksrufkim100@gmail.com>
2022-12-31 06:58:32 +0000

    simple JWT

---
+++ aa/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('sjwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('sjwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
